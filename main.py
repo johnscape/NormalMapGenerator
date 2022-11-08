a88a42d2d7e2be97d9bad1f6b72f249af868b8d9
Attila Baczay <johnscape96@gmail.com>
2022-11-08 11:08:35 +0100

    Checking different image sizes

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+import random
+
+import numpy as np
+import cv2
 from ImageProcessor import ImageProcessor
 from Dataset import Dataset
+from Network import NormalGeneratorNetwork
 
-processor = ImageProcessor(verbose=2)
-processor.ProcessImages(32, 32)
-processor.CreateTestingSet()
+imageSizes = [16, 32, 64, 128]
 
-dataset = Dataset("/work", 100, False, True)
+for imageSize in imageSizes:
+    processor = ImageProcessor()
+    processor.ProcessImages(imageSize, imageSize)
+    processor.CreateTestingSet()
+
+    trainingDataset = Dataset("/work", 1000, True, True)
+    testingDataset = Dataset("/work", 100, True, False)
+
+    network = NormalGeneratorNetwork("work", imageSize, trainingDataset, testingDataset)
+    network.CreateModel()
+    network.Train()
+
+    imageNumber = random.randint(0, len(testingDataset.Dataset[0]))
+    inputImage = np.asarray(testingDataset.Dataset[0][imageNumber])
+    expectedImage = np.asarray(testingDataset.Dataset[1][imageNumber])
+    generatedImage = network.Predict(inputImage.reshape((1, imageSize, imageSize, 3))).reshape((imageSize, imageSize, 3))
+
+    inputImage = np.round(inputImage * 255, decimals=0).astype('uint8')
+    expectedImage = np.round(expectedImage * 255, decimals=0).astype('uint8')
+    generatedImage = np.round(generatedImage * 255, decimals=0).astype('uint8')
+
+    cv2.imwrite("work/rgb_" + str(imageSize) + ".png", cv2.cvtColor(inputImage, cv2.COLOR_RGB2BGR))
+    cv2.imwrite("work/normal_" + str(imageSize) + ".png", cv2.cvtColor(expectedImage, cv2.COLOR_RGB2BGR))
+    cv2.imwrite("work/generated_" + str(imageSize) + ".png", cv2.cvtColor(generatedImage, cv2.COLOR_RGB2BGR))
