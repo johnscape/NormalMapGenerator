9e226e75e43f386a619e51937e1a878ecc88204c
Attila Baczay <johnscape96@gmail.com>
2022-12-09 14:11:35 +0100

    Added reflecion padding

--- Network.py
+++ Network.py
@@ -2,11 +2,24 @@ import os.path
 import matplotlib.pyplot as plt
 import keras.models
 from keras.models import Model
-from keras.layers import Input, Conv2D, AveragePooling2D, UpSampling2D, BatchNormalization
+from keras.layers import Input, Conv2D, AveragePooling2D, UpSampling2D, BatchNormalization, Layer, InputSpec
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 import numpy as np
+import tensorflow as tf
 from Dataset import Dataset
+
+class ReflectionPadding2D(Layer):
+    def __init__(self, padding=(1, 1), **kwargs):
+        self.padding = tuple(padding)
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[1] + 2 * self.padding[0], input_shape[2] + 2 * self.padding[1], input_shape[3])
+
+    def call(self, input_tensor, mask=None):
+        padding_width, padding_height = self.padding
+        return tf.pad(input_tensor, [[0,0], [padding_height, padding_height], [padding_width, padding_width], [0,0] ], 'REFLECT')
 
 
 class NormalGeneratorNetwork:
